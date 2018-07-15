efc299633b24ac002bcdb81fe4ce116470481c8c
SophieAlSaffar <Sophie.alsaffar@gmail.com>
2018-07-15 19:24:36 +0300

    the pages project

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Page
 
 # Create your views here.
+def page_list(request):
+    pages = Page.objects.all()
+    context = {
+    "pages":pages,
+
+    }
+    return render (request, "list.html", context)
+
+def page_detail(request, page_id):
+    page= Page.objects.get(id=page_id)
+
+    context = {
+        "page":page,
+    }
+    return render(request, 'detail.html', context)
--- wiki/urls.py
+++ wiki/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pages import views
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pages/list',views.page_list, name='page-list'),
+    path('pages/<int:page_id>/detail',views.page_detail, name='page-detail'),
+
 ]
